674cf68c781e55d3a8739d5ce3d3bb59d966324a
Jeremy Saenz <jeremy.saenz@gmail.com>
2022-06-22 13:04:45 -0700

    test: adding basic testing harness and missing listers

--- pkg/channel/jetstream/controller/reconciler_test.go
+++ pkg/channel/jetstream/controller/reconciler_test.go
@@ -1,17 +1,35 @@
+/*
+Copyright 2019 The Knative Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package controller
 
 import (
 	"context"
 	"testing"
 
-	"knative.dev/eventing-natss/pkg/apis/messaging/v1beta1"
-	"knative.dev/eventing-natss/pkg/client/clientset/versioned/scheme"
-	"knative.dev/eventing-natss/pkg/client/injection/reconciler/messaging/v1beta1/natsschannel"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/logging"
-
 	. "knative.dev/pkg/reconciler/testing"
+
+	"knative.dev/eventing-natss/pkg/apis/messaging/v1beta1"
+	"knative.dev/eventing-natss/pkg/client/clientset/versioned/scheme"
+	fakeclientset "knative.dev/eventing-natss/pkg/client/injection/client/fake"
+	"knative.dev/eventing-natss/pkg/client/injection/reconciler/messaging/v1alpha1/natsjetstreamchannel"
+	reconciletesting "knative.dev/eventing-natss/pkg/reconciler/testing"
 )
 
 func init() {
@@ -29,7 +47,7 @@ const (
 )
 
 func TestAllCases(t *testing.T) {
-	ncKey := testNS + "/" + ncName
+	// ncKey := testNS + "/" + ncName
 	table := TableTest{
 		{
 			Name: "bad workqueue key",
@@ -40,16 +58,20 @@ func TestAllCases(t *testing.T) {
 
 	table.Test(t, reconciletesting.MakeFactory(func(ctx context.Context, listers *reconciletesting.Listers) controller.Reconciler {
 		r := &Reconciler{
-			dispatcherNamespace:      testNS,
-			dispatcherDeploymentName: dispatcherDeploymentName,
-			dispatcherServiceName:    dispatcherServiceName,
-			kubeClientSet:            fakekubeclient.Get(ctx),
+			kubeClientSet:            nil,
+			systemNamespace:          "",
+			dispatcherImage:          "",
+			dispatcherServiceAccount: "",
 			deploymentLister:         listers.GetDeploymentLister(),
 			serviceLister:            listers.GetServiceLister(),
 			endpointsLister:          listers.GetEndpointsLister(),
+			serviceAccountLister:     listers.GetServiceAccountLister(),
+			roleBindingLister:        listers.GetRoleBindingLister(),
+			//TODO: Figure out controllerRef
+			// controllerRef:            v1.OwnerReference{},
 		}
-		return natsschannel.NewReconciler(ctx, logging.FromContext(ctx),
-			fakeclientset.Get(ctx), listers.GetNatssChannelLister(),
+		return natsjetstreamchannel.NewReconciler(ctx, logging.FromContext(ctx),
+			fakeclientset.Get(ctx), listers.GetNatsJetstreamChannelLister(),
 			controller.GetEventRecorder(ctx),
 			r)
 	}))
